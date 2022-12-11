de072cb5de889e166502b18137d70042933b8b34
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-12-11 06:24:09 -0300

    wip: adiciona parte inicial do card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import {VStack} from '@chakra-ui/react'
+import CardMain from "./components/cardMain";
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App" style={{ color: "red" }}>
-      Componente app
+    <div className="App">
+      <VStack width='600px'>
+        <CardMain/>
+      </VStack>
     </div>
   );
 }
