f9efdcc9bafc2e3acca97ca724f3c6c4402f4550
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-12-15 19:47:39 -0300

    style: adiciona tema customizado ao chakra

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import AppRoutes from "./routes/Routes";
+import {theme} from './theme/index'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AppRoutes />
     </ChakraProvider>
   </React.StrictMode>
