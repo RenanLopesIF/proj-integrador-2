4c47cc9ba785b863ec162c9a7883698df81d48e3
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-12-16 09:34:16 -0300

    chore: eslint | prettier | editorconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:import/recommended', 'eslint-config-prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  rules: {
+    'import/named': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-constant-binary-expression': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-else-if': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-imports': 'error',
+    'no-use-before-define': 'error',
+    camelcase: 'warn',
+  },
+};
