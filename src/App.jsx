e22d32eac306ff4a6c56aa5d68ae335d80a82ff6
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-12-16 09:38:39 -0300

    refactor: altera nome do component Card -> Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import {VStack} from '@chakra-ui/react'
-import CardMain from "./components/cardMain";
-import "./App.css"
+import React, { useState } from 'react';
+import { VStack } from '@chakra-ui/react';
+import Post from './components/Post';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <VStack width='600px'>
-        <CardMain/>
+      <VStack width="600px">
+        <Post />
       </VStack>
     </div>
   );
---
+++ src/components/Post.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Box, HStack, Avatar, Text, Badge, Flex, VStack, Image, Center, Button } from '@chakra-ui/react';
+import { MdAlarmOff, MdAlarmOn } from 'react-icons/md';
+import { BiMap } from 'react-icons/bi';
+import Faixaetaria from './FaixaEtaria';
+
+function Post(props) {
+  const {} = props;
+  const username = 'Renan Lopes';
+  const postTitle = 'Festa das Cores';
+  const postDescription =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, at? Voluptatum cum quod eius magnam asperiores consequatur, consequuntur, tempora provident rerum quis perferendis! Nihil enim reprehenderit impedit aut corporis? Culpa.';
+  const postImageUrlx =
+    'https://valenews.com.br/wp-content/uploads/2021/10/FestivaldasCoresNovaGokula27102021FernandoNoronha.jpg';
+  const postImageUrl = 'https://pbs.twimg.com/media/ETCnTxDX0AIsIXG?format=jpg&name=900x900';
+  const contentPadding = 3;
+  const interesedCount = 2;
+  const commentCount = 5;
+
+  return (
+    <Box borderRadius="10px" bgColor="#FFF" width="100%" minH="50px">
+      <HStack alignItems="flex-start" justifyContent="space-between" w="full" padding={contentPadding}>
+        <HStack>
+          <Avatar />
+          <Flex flexDir="column" gap={1}>
+            <Text color="black" fontSize={16}>
+              {username}
+            </Text>
+            <Text color="gray.500" fontSize={14}>
+              1 dia atrás
+            </Text>
+          </Flex>
+        </HStack>
+        <Flex alignItems="flex-end" flexDir="column" gap={1}>
+          <Badge padding={1} colorScheme="yellow">
+            Acontecendo agora
+          </Badge>
+        </Flex>
+      </HStack>
+      <VStack
+        alignItems="flex-start"
+        px={contentPadding * 2}
+        py={contentPadding}
+        backgroundColor="#364f6b"
+        width="100%"
+        gap={2}
+      >
+        <HStack gap={6}>
+          <HStack>
+            <MdAlarmOn color="#F5F5F5" />
+            <Text fontWeight={500} color="#F5F5F5" fontSize={12}>
+              10/12/2022 - 20:30
+            </Text>
+          </HStack>
+          <HStack>
+            <MdAlarmOff color="#F5F5F5" />
+            <Text fontWeight={500} color="#F5F5F5" fontSize={12}>
+              11/12/2022 - 3:00
+            </Text>
+          </HStack>
+        </HStack>
+        <HStack>
+          <BiMap color="#F5F5F5" />
+          <Text fontWeight={500} color="#F5F5F5" fontSize={12}>
+            EM ARAÇUAÍ, À 27KM
+          </Text>
+        </HStack>
+      </VStack>
+      <Box padding={contentPadding}>
+        <HStack mt={2}>
+          <Text fontWeight={700} color="#364F6B">
+            {postTitle}
+          </Text>
+          <Faixaetaria label="+16" color="red.500" />
+        </HStack>
+        <Box>
+          <Text color="#364F6b" fontSize={14}>
+            {postDescription}
+          </Text>
+          <Center mt={4}>
+            <Image maxH="400px" src={postImageUrl} />
+          </Center>
+          <Flex justifyContent="flex-end" alignItems="center" mt={7}>
+            <Text fontSize={12}>
+              {interesedCount} {interesedCount === 1 ? 'interessado' : 'interessados'}
+              {' e '}
+              {commentCount} {commentCount === 1 ? 'comentário' : 'comentários'}
+            </Text>
+          </Flex>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Button color="primary.300" variant="unstyled" h="10" w="full">
+              INTERESSEI
+            </Button>
+            <Box h="7" w="1" bgColor="primary"></Box>
+            <Button color="primary.300" variant="unstyled" h="10" w="full">
+              COMENTÁRIOS
+            </Button>
+            <Box h="7" w="1" bgColor="primary"></Box>
+            <Button color="primary.300" variant="unstyled" h="10" w="full">
+              LOCALIZAÇÃO
+            </Button>
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Post;
