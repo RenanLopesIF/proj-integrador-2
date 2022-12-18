1824521ef1f1c1b9927be1b7972a9b6cebe4792e
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-12-18 00:55:59 -0300

    fix(EXEMPLOS): arruma import do componente UsoDoChildren

--- src/routes/Tutoriais.Routes.jsx
+++ src/routes/Tutoriais.Routes.jsx
@@ -1,26 +1,21 @@
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 
-import MeuComponenteAvancado from "../EXEMPLOS/avancado";
-import MeuComponenteSimples from "../EXEMPLOS/simples";
-import MeuComponenteIntermediario from "../EXEMPLOS/intermediario";
-import UsoDoChildren from "../EXEMPLOS/usoDoChildren";
-import Tutoriais from "../EXEMPLOS/tutoriais";
+import MeuComponenteAvancado from '../EXEMPLOS/avancado';
+import MeuComponenteSimples from '../EXEMPLOS/simples';
+import MeuComponenteIntermediario from '../EXEMPLOS/intermediario';
+import UsoDoChildren from '../EXEMPLOS/UsoDoChildren';
+import Tutoriais from '../EXEMPLOS/tutoriais';
 
 function TutorialsRoutes() {
-  const listaDePessoas = ["Amaury", "Luan", "Renan", "Vitor"];
+  const listaDePessoas = ['Amaury', 'Luan', 'Renan', 'Vitor'];
 
   return [
     <Route path="/tutoriais" element={<Tutoriais />} />,
     <Route
       path="/tutorial/avancado"
-      element={
-        <MeuComponenteAvancado titulo="Lista" listaDePessoas={listaDePessoas} />
-      }
+      element={<MeuComponenteAvancado titulo="Lista" listaDePessoas={listaDePessoas} />}
     />,
-    <Route
-      path="/tutorial/intermediario"
-      element={<MeuComponenteIntermediario />}
-    />,
+    <Route path="/tutorial/intermediario" element={<MeuComponenteIntermediario />} />,
     <Route path="/tutorial/simples" element={<MeuComponenteSimples />} />,
     <Route path="/tutorial/usoDoChildren" element={<UsoDoChildren />} />,
   ];
