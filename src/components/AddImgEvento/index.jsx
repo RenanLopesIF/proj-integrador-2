11e846d4f27ae7bc0c949a258641e63a8dd3e863
1uanss <sousasluan212@gmail.com>
2023-01-11 21:46:49 -0300

    fix alteracao do width e height

--- src/components/AddImgEvento/index.jsx
+++ src/components/AddImgEvento/index.jsx
@@ -13,7 +13,7 @@ import { Box, Image, Link } from '@chakra-ui/react'
     }
 
     return (
-      <Box maxW='sm' w='15rem' h='14rem' borderWidth='1px' borderRadius='lg' gap='0.9rem' bgColor='#D9D9D9' flexDirection='column' alignItems='center' justifyContent='center' display='flex'  overflow='hidden'>
+      <Box maxW='sm' w='100%' h='100%' borderWidth='1px' borderRadius='lg' gap='0.9rem' bgColor='#D9D9D9' flexDirection='column' alignItems='center' justifyContent='center' display='flex'  overflow='hidden'>
       <Image src={imageUrl} border='1px' boxSize='120' borderRadius='0.9rem' />
         <Link  href='#'  _hover={{textDecoration:'none'}}  w='14rem' h='2rem' display='flex' justifyContent={'center'} alignItems={'center'} borderRadius={'5px'}  fontSize={'12px'} gap={'10px'} style={property} >{text}
         </Link>
