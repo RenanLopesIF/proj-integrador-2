c49a00364f06bc3a3d8e7aefc1b4ecf10e810535
1uanss <sousasluan212@gmail.com>
2023-01-16 09:36:44 -0300

    fix colocando css direto na box

--- src/TESTES/Luan/index.jsx
+++ src/TESTES/Luan/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Circle } from '@chakra-ui/react';
+import { Box, Circle, Flex } from '@chakra-ui/react';
 import React from 'react';
 import ButtonSubmit from '../../components/ButtonSubmit';
 // import CustomInput from '../../components/CustomInput';
@@ -6,34 +6,6 @@ import InputSenha from '../../components/InputSenha';
 
 // import { MdSettings } from 'react-icons/md';
 // Se der ruim, apaga tudo e coloca return <div></div>
-const father = {
-  // backgroundColor: 'red',
-  with: '100%',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  // backgroundColor: 'red',
-  alignItems: 'center',
-  color: 'black',
-};
-const componentes = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'flex-start',
-  width: '40vw',
-  height: '80vh',
-  backgroundColor: 'white',
-};
-const text = {
-  display: 'flex',
-  justifyContent: 'end',
-  flexDirection: 'column',
-  width: '100%',
-  height: '30%',
-  // backgroundColor: 'gray',
-  // fontSize: '20px',
-};
 
 const txtElogo = {
   display: 'flex',
@@ -59,22 +31,30 @@ const txtEchave = {
   gap: '2rem',
 };
 
-const inputs = {
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  fontSize: '1.2rem',
-  width: '100%',
-  height: '30%',
-  // backgroundColor: 'blue',
-  gap: '70px',
-};
+// const father = {
+//   // backgroundColor: 'red',
+//   with: '100%',
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   // backgroundColor: 'red',
+//   alignItems: 'center',
+//   color: 'black',
+// };
 
 function TesteLuan() {
   return (
-    <div style={father}>
-      <Box style={componentes}>
-        <Box style={text}>
+    <Flex w={'100%'} h={'100vh'} justifyContent={'center'} alignItems={'center'} color={'black'}>
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'center'}
+        alignItems={'flex-start'}
+        w={'40vw'}
+        h={'80vh'}
+        backgroundColor={'white'}
+      >
+        <Box display={'flex'} justifyContent={'end'} flexDirection={'column'} w={'100%'} h={'30%'}>
           <h1 style={txtElogo}>
             <img src="../../../public/logo.svg" alt="" /> LOGO MARCA
           </h1>
@@ -82,11 +62,20 @@ function TesteLuan() {
             <img src="../../../public/chave.svg" alt="" /> Recuperar sua senha
           </h1>
         </Box>
-        <Box style={inputs}>
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          flexDirection={'column'}
+          fontSize={'1.2rem'}
+          w="100%"
+          h="40%"
+          gap="60px"
+          backgroundColor="blue"
+        >
           <InputSenha text={'Nova senha'} placeholder={'4 ou mais caracteres'} />
           <InputSenha text={'Confirme senha'} placeholder={'Mesma que a nova senha acima'} />
         </Box>
-        <Box display={'flex'} w={'100%'} h={'30%'} alignItems={'center'} paddingLeft={'2rem'}>
+        <Box backgroundColor={'red'} display={'flex'} w={'100%'} h={'30%'} alignItems={'center'} paddingLeft={'2rem'}>
           <ButtonSubmit text={'Recuperar'} color={'#FFF'} background={'#FC5185'} />
           <Box display={'flex'} marginTop="2.8rem" justifyContent="space-around" w="140px">
             <Circle w={'1.4rem'} h={'1.4rem'} background={'#FC5185'} />
@@ -95,7 +84,7 @@ function TesteLuan() {
           </Box>
         </Box>
       </Box>
-    </div>
+    </Flex>
   );
 }
 
