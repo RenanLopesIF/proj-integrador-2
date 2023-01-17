b21c4954eff89a20e2de7894b9fa582b5205537b
devamauryjunior <jobagpj@gmail.com>
2023-01-16 21:44:38 -0300

    feat: add and start page Publications

---
+++ src/pages/Publications/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import BarraPesquisa from "./../../components/BarraPesquisa"
+function Publications() {
+  return (
+    <Box>
+      <BarraPesquisa text="Pesquise pela descrição do evento"/>
+
+    </Box>
+  )
+}
+
+export default Publications;
