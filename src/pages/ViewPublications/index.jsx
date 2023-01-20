604e3e1c5b589343a13ab22732379b58099a4307
devamauryjunior <jobagpj@gmail.com>
2023-01-20 19:40:04 -0300

    feat: fineshed

--- src/pages/ViewPublications/index.jsx
+++ src/pages/ViewPublications/index.jsx
@@ -1,4 +1,4 @@
-import { Center, Flex, Box } from '@chakra-ui/react';
+import { Center, Flex, Box, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import LoadingSpinner from '../../components/LoadingSpinner';
@@ -10,13 +10,15 @@ function ViewPublications() {
 
   const [loading, setLoading] = useState(true);
 
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     async function getEvent() {
       try {
         const data = await axios.get('http://localhost:3005/events');
         setEvents(data.data);
       } catch (error) {
-        console.log(error);
+        setError(true);
       } finally {
         setLoading(false);
       }
@@ -27,24 +29,36 @@ function ViewPublications() {
 
   if (loading)
     return (
-      <Center>
+      <Center w="100%" h="100%">
         <LoadingSpinner />
       </Center>
     );
+  if (error) {
+    return (
+      <Center w="100%" h="100%">
+        <Text fontWeight="900" fontSize="30px">
+          Desculpe, algo deu erro:)
+        </Text>
+      </Center>
+    );
+  }
 
   return (
-    <>
-      <Flex alignItems="flex-end">
-        <IconUsuario />
+    <Box padding="20px 25px 26px 25px">
+      <Flex textAlign="right" alignItems="center" justifyContent="space-between">
+        ,
+        <Box>
+          <IconUsuario />
+        </Box>
       </Flex>
       <Center>
-        <Flex w="578px" flexDirection="column">
+        <Flex w="578px" flexDirection="column" gap="20px">
           {events.map((event) => (
             <Post event={event} />
           ))}
         </Flex>
       </Center>
-    </>
+    </Box>
   );
 }
 
