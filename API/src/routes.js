93d2c3e40b874d3a87a5033eea1174887ef3ca13
Victor Hugo <vhlcads@gmail.com>
2023-02-01 19:44:08 -0300

    Criando API do login de usuário

--- API/src/routes.js
+++ API/src/routes.js
@@ -11,6 +11,9 @@ routes.use(json());
 routes.get('/usuarios', usuariosController.getAll);
 routes.get('/usuarios/:id', usuariosController.getOne);
 routes.post('/usuarios/novo', usuariosController.insertOne);
+routes.get('/usuario/evento/:userID', eventosController.getByUser);
+routes.get('/evento/:eventID', eventosController.getOne);
+routes.get('/eventos', eventosController.getAll);
 
 routes.post('/authenticate',LoginUserController.authenticate)
 export default routes;
