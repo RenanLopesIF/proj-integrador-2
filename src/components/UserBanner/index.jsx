70f0e235a9e729895038325605eb262c65c0489e
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2023-02-23 22:32:28 -0300

    refactor: remove console.log

--- src/components/UserBanner/index.jsx
+++ src/components/UserBanner/index.jsx
@@ -2,7 +2,6 @@ import { Center, Image } from '@chakra-ui/react';
 import React from 'react';
 
 function UserBanner({ src }) {
-  console.log('a', src);
   return (
     <Center w="full" h="180px" bgColor="secondary.800">
       {src && <Image src={src} maxH="full" />}
